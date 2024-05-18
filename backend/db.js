59c89e1623589a789333c2ead4333dd1750665c5
anshit2526 <anshitsiddharth2003@gmail.com>
2024-05-18 23:23:05 +0530

    Established connection with MongoDB database

---
+++ backend/db.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+
+const mongoURI = "mongodb://localhost:27017/"
+
+
+const connectToMongo = async () => {
+    try {
+        mongoose.set('strictQuery', false);
+        mongoose.connect(mongoURI);
+        console.log("Connected to Mongo Successfully.");
+    }
+    catch (error) {
+        console.log(error)
+        process.exit()
+    }
+}
+
+module.exports = connectToMongo;
