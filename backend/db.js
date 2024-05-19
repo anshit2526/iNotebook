dae53bf873ab804edbc4f469202eb90a99740fd0
anshit2526 <anshitsiddharth2003@gmail.com>
2024-05-19 21:29:00 +0530

    Established connections to store data into the database

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 
-const mongoURI = "mongodb://localhost:27017/"
+const mongoURI = "mongodb://localhost:27017/?readPreference=primary&directConnection=true&tls=false"
 
 
 const connectToMongo = async () => {
--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,16 @@
 const connectToMongo = require('./db');
 const express = require('express');
 
+connectToMongo();
 const app = express()
 const port = 3000
 
-connectToMongo();
+// A middleware to use req.body in this project. This is used to deal with json like sending requests in json.  
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello Anshit!')
-})
+// Available Routes 
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/notes', require('./routes/notes'))
 
 app.listen(port, () => {
   console.log(`Example app listening on port https://localhost:${port}/`)
