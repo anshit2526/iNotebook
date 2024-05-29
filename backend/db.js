81ca1fa2de6fc64f249feb3143484a8fc8162561
anshit2526 <anshitsiddharth2003@gmail.com>
2024-05-29 22:45:09 +0530

    Added Data Validation in the backend to ensure only valid data would be stored.

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 
-const mongoURI = "mongodb://localhost:27017/?readPreference=primary&directConnection=true&tls=false"
+const mongoURI = "mongodb://localhost:27017/inotebook?readPreference=primary&directConnection=true&tls=false"
 
 
 const connectToMongo = async () => {
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,14 +1,32 @@
 const express = require('express');
 const User = require('../models/User');
 const router = express.Router();
+const { body, validationResult } = require('express-validator');
 
 // Creating a User using POST: "api/auth/". Doesn't require Auth
-router.post('/', (req, res)=>{
+router.post('/', [
+    // all the validators will come here
+    body('name', 'Enter a valid name').isLength({ min: 3 }),
+    body('email', 'Enter a valid email').isEmail(),
+    body('password', 'Password must be atleast 5 characters long.').isLength({ min: 5 }),
+], (req, res) => {
 
-    console.log(req.body);
-    const user = User(req.body);
-    user.save();
-    res.send(req.body);
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ erros: errors.array() });
+    }
+
+    User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+
+    }).then(user => res.json(user))
+        .catch((err) => {
+            console.log("\n" + err)
+            console.log("\nUser with", req.body.email, "already exist");
+            res.json({ error: `User with ${req.body.email} already exist`, message: err.message });
+        })
 
 })
 
