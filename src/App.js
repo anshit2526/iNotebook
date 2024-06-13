04ad08a2660e0a3a9c176c94392eeee166e03e0a
anshit2526 <anshitsiddharth2003@gmail.com>
2024-06-14 03:26:16 +0530

    Takes first step towards front-end development. Introduced routers.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, } from "react-router-dom";
+import { Navbar } from './components/Navbar';
+import { Home } from './components/Home';
+import { About } from './components/About';
+
+
+
 
 function App() {
+
   return (
-    <div className="App">
-      <h1>Welcome to iNotebook</h1>
-    </div>
+    <>
+
+      <Router>
+
+        <Navbar />
+
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/about" element={<About />} />
+
+        </Routes>
+      </Router>
+
+    </>
   );
 }
 
