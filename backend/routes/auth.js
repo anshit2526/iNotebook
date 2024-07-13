e4b33da9d7df2ee1bdfc595c7c4bea33b4565fe4
anshit2526 <anshitsiddharth2003@gmail.com>
2024-07-14 00:27:08 +0530

    Login and Signup support

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -66,6 +66,8 @@ router.post('/login', [
     body('password', 'Password cannot be blank').exists()
 ], async (req, res) => {
 
+    let success = false;
+
     // If there are errors, return Bad request and the errors
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
@@ -81,7 +83,7 @@ router.post('/login', [
 
         const passwordCompare = await bcrypt.compare(password, user.password);
         if (!passwordCompare) {
-            return res.status(400).json({ error: 'Please login with correct credentials' });
+            return res.status(400).json({ success, error: 'Please login with correct credentials' });
         }
 
         const data = {
@@ -91,7 +93,8 @@ router.post('/login', [
         }
 
         const authToken = jwt.sign(data, JWT_SECRET);
-        res.json({ authToken })
+        success = true;
+        res.json({ success, authToken })
 
     } catch (error) {
         console.error(error.message);
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,11 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
 
 export const Navbar = () => {
 
     // This line of code is helps to highliging the active tabs in navbar.
     let location = useLocation();
-    
+
     return (
         <nav className="navbar navbar-expand-lg bg-dark navbar-dark">
             <div className="container">
@@ -23,8 +23,8 @@ export const Navbar = () => {
                         </li>
                     </ul>
                     <form className="d-flex" role="search">
-                        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                        <button className="btn btn-outline-success" type="submit">Search</button>
+                        <Link className="btn btn-primary btn-sm mx-2" to="/login" role="button">Login</Link>
+                        <Link className="btn btn-primary btn-sm mx-2" to="/signup" role="button">Signup</Link>
                     </form>
                 </div>
             </div>
